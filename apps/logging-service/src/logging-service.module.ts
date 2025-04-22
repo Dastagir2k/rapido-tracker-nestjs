a1a1aec91fc0e999c84c6281412352d69ec88aa6
unknown <dastagir2k@gmail.com>
2025-04-22 09:57:11 +0530

    initialize and set up MongoDB connection for logging service

--- apps/logging-service/src/logging-service.module.ts
+++ apps/logging-service/src/logging-service.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LoggingServiceController } from './logging-service.controller';
 import { LoggingServiceService } from './logging-service.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://das:LoggingService@cluster0.zkyukie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')],
   controllers: [LoggingServiceController],
   providers: [LoggingServiceService],
 })
